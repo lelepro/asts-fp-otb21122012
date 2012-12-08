235a86185609488e5569c533a547dbd80571064e
huychuongctt@gmail.com <huychuongctt@gmail.com@66ed9901-2adb-79b9-802d-a2435753cfd7>
2012-12-08 12:29:04 +0000

    Tao project

---
+++ trunk/BayReNhat/BayReNhat/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BayReNhat
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
