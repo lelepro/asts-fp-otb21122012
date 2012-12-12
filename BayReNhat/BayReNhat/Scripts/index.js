3d2f8be67c3446d700d1828d5654de0f80c07aaa
huychuongctt <huychuongctt@gmail.com>
2012-12-12 20:10:38 +0000

    Complete choose depart city and return city

--- BayReNhat/BayReNhat/Scripts/index.js
+++ BayReNhat/BayReNhat/Scripts/index.js
@@ -1,3 +1,45 @@
 ﻿$(function () {
-    $("#departday, #returnday").datepicker();
+	init();
+	registerdlg("#dlgCity", "#departcity");
+	registerdlg("#dlgCity", "#returncity");
 });
+
+function init() {
+	$("#departday, #returnday").datepicker();
+	$("#dlgCity").dialog({
+		autoOpen: false,
+		draggable: false,
+		resizable: false,
+	});
+	$("#dlgCity").selectable({
+		filter: "li",
+		selected: function (event, ui) {
+			$(ui.selected).addClass("ui-selected").siblings().removeClass("ui-selected");
+		}
+	});
+}
+
+function registerdlg(dlgId, targetId) {
+	$(targetId).click(function () {
+		$(dlgId).selectable("option", {
+			stop:
+				function() {
+					$(targetId).val($(".ui-selected").text());
+				}
+		});
+		$(dlgId).dialog("option", {
+			position: { my: "left top", at: "left bottom", of: targetId },
+			open: function() {
+				var selected = $(".ui-widget-content .ui-selected");
+				if (selected != null) {
+					selected.removeClass("ui-selected");
+				}
+			}
+		});
+		$(dlgId).dialog("open");
+		$(targetId).focus();
+	});
+	$(targetId).blur(function () {
+		$(dlgId).dialog("close");
+	});
+}
