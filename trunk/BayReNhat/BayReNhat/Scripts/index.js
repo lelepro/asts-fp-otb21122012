ef3f9fcd6c4abc5bd91a0fcc32d8eda767ac79aa
huychuongctt@gmail.com <huychuongctt@gmail.com@66ed9901-2adb-79b9-802d-a2435753cfd7>
2012-12-15 11:38:14 +0000

    Add validation for index page Huy Chuong

--- trunk/BayReNhat/BayReNhat/Scripts/index.js
+++ trunk/BayReNhat/BayReNhat/Scripts/index.js
@@ -2,10 +2,16 @@
 	init();
 	registerdlg("#dlgCity", "#departcity");
 	registerdlg("#dlgCity", "#returncity");
-	hintTextbox("#departcity", "Bạn đi từ đâu");
-	hintTextbox("#returncity", "Bạn muốn đến đâu");
-	hintTextbox("#departday", "Ngày nào bạn đi");
-	hintTextbox("#returnday", "Ngày nào bạn về");
+	hintTextbox("#departcity", "bạn đi từ đâu");
+	hintTextbox("#returncity", "bạn muốn đến đâu");
+	hintTextbox("#departday", "ngày nào bạn đi");
+	hintTextbox("#returnday", "ngày nào bạn về");
+	$("input[name=direction]:radio").change(function() {
+		if ($("input[name=direction]:checked").val() == "onewaytravel")
+			$("#returnday").attr("disabled", true);
+		else if ($("input[name=direction]:checked").val() == "returntravel")
+			$("#returnday").attr("disabled", false);
+	});
 });
 
 function init() {
@@ -52,17 +58,50 @@ function hintTextbox(textboxId, hint) {
 	var textbox = $(textboxId);
 	textbox.val(hint);
 	textbox.css("color", "gray");
+	textbox.attr("isselected", false);
 	
 	textbox.click(function() {
 		if (textbox.val() == hint) {
 			textbox.val("");
 			textbox.css("color", "");
+			textbox.attr("isselected", true);
 		}
 	});
 	textbox.change(function() {
 		if (textbox.val() == "") {
 			textbox.val(hint);
 			textbox.css("color", "gray");
+			textbox.attr("isselected", false);
 		}
 	});
+}
+
+// Validation
+$(function() {
+	$("#btnsearch").click(function() {
+		$(".error").empty();
+		var err = false;
+		if ($("#departcity").attr("isselected") == "false") {
+			showError("Bạn chưa chọn điểm đi");
+			err = true;
+		}
+		if ($("#returncity").attr("isselected") == "false") {
+			showError("Bạn chưa chọn điểm đến");
+			err = true;
+		}
+		if ($("#departday").attr("isselected") == "false") {
+			showError("Bạn chưa chọn ngày đi");
+			err = true;
+		}
+		if ($("#returnday").attr("isselected") == "false" && 
+			$("#returnday").attr("disabled")) {
+			showError("Bạn chưa chọn ngày về");
+			err = true;
+		}
+		return !err;
+	});
+});
+
+function showError(msg) {
+	$(".error").append("<li>"+msg+"</li>");
 }
